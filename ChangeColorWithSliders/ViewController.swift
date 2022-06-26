c36fda4acad61e27cf93146fdf88ec7dd8fdbb1b
Vladimir <vproshchin@gmail.com>
2022-06-26 17:35:08 +0300

    Deleted shit

--- ChangeColorWithSliders/ViewController.swift
+++ ChangeColorWithSliders/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var rectangleView: UIView!
-    
+
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
@@ -19,31 +19,25 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    let defaultValue: Float = 50.0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         rectangleView.layer.cornerRadius = 10
     }
     
+    
+    
     @IBAction func changeRedColor() {
-        redValueLabel.text = "\(redSlider.value.rounded())"
-        if redSlider.value.rounded() == defaultValue {
-            redValueLabel.text = "Default(50)"
-        }
+        redValueLabel.text = "\(redSlider.value)"
     }
     
     @IBAction func changeGreenColor() {
-        greenValueLabel.text = "\(greenSlider.value.rounded())"
-        if greenSlider.value.rounded() == defaultValue {
-            greenValueLabel.text = "Default(50)"
-        }
+        greenValueLabel.text = "\(greenSlider.value)"
     }
     
     @IBAction func changeBlueColor() {
-        blueValueLabel.text = "\(blueSlider.value.rounded())"
-        if blueSlider.value.rounded() == defaultValue {
-            blueValueLabel.text = "Default(50)"
-        }
+        blueValueLabel.text = "\(blueSlider.value)"
     }
+    
 }
