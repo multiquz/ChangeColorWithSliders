bd0ed20b3f94f19ec0d846623f325c2b54774c27
Vladimir <vproshchin@gmail.com>
2022-06-26 16:42:05 +0300

    Added logic to label.text change

--- ChangeColorWithSliders/ViewController.swift
+++ ChangeColorWithSliders/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var rectangleView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    let defaultValue: Float = 50.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rectangleView.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func changeRedColor() {
+        redValueLabel.text = "\(redSlider.value.rounded())"
+        if redSlider.value.rounded() == defaultValue {
+            redValueLabel.text = "Default(50)"
+        }
+    }
+    
+    @IBAction func changeGreenColor() {
+        greenValueLabel.text = "\(greenSlider.value.rounded())"
+        if greenSlider.value.rounded() == defaultValue {
+            greenValueLabel.text = "Default(50)"
+        }
+    }
+    
+    @IBAction func changeBlueColor() {
+        blueValueLabel.text = "\(blueSlider.value.rounded())"
+        if blueSlider.value.rounded() == defaultValue {
+            blueValueLabel.text = "Default(50)"
+        }
+    }
 }
-
