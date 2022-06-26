b9c1e25b84e28fa9d6558539f51b3367fd825ef2
Vladimir <vproshchin@gmail.com>
2022-06-26 17:45:57 +0300

    Added change color to view. The app works.

--- ChangeColorWithSliders/ViewController.swift
+++ ChangeColorWithSliders/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IB Outlets
     @IBOutlet var rectangleView: UIView!
-
+    
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
@@ -20,24 +21,34 @@ class ViewController: UIViewController {
     @IBOutlet var blueSlider: UISlider!
     
     
-    
+    // MARK: Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         rectangleView.layer.cornerRadius = 10
     }
     
-    
-    
+    // MARK: IB Actions
     @IBAction func changeRedColor() {
         redValueLabel.text = "\(redSlider.value)"
+        setRectangleViewColor()
     }
     
     @IBAction func changeGreenColor() {
         greenValueLabel.text = "\(greenSlider.value)"
+        setRectangleViewColor()
     }
     
     @IBAction func changeBlueColor() {
         blueValueLabel.text = "\(blueSlider.value)"
+        setRectangleViewColor()
     }
     
+    // MARK: Private Methods
+    private func setRectangleViewColor() {
+        rectangleView.backgroundColor = UIColor.init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
 }
